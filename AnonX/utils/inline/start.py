df7969cd7308ca7843b02d91f6445ca7d94e4454
Vivekkumar07089 <155367948+Vivekkumar07089@users.noreply.github.com>
2024-01-06 19:10:01 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,23 +9,23 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌹𝐀ԃ𝐃 𝐌ҽ 𝐌σ𝐈 𝐋ꪮꪜ𝐄🌹",
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="❤️𝐎ɯɳҽ𝐑❤️",
-                url=f"https://t.me/your_godfather_xd",
+                text="ᴏᴡɴᴇʀ",
+                user_id=OWNER,
             )
         ],
         [
             InlineKeyboardButton(
-                text="💖𝐇ҽʅ𝐏💖",
+                text="ʜᴇʟᴘ",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🌸𝐒ҽƚƚιɳɠ𝐒🌸", callback_data="settings_helper"
+                text="sᴇᴛᴛɪɴɢ's", callback_data="settings_helper"
             ),
         ],
      ]
@@ -36,24 +36,24 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="•🌷𝐀ԃ𝐃 𝐌ҽ 𝐓σ 𝐘συ𝐑 𝐆ɾσυ𝐏🌷•",
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🍒𝐂σɱɱαɳԃ𝐒🍒", callback_data="settings_back_helper"
+                text="ᴄᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper"
             ),
             InlineKeyboardButton(
-                text="🥀𝐅συɳԃҽ𝐑🥀", user_id=OWNER
+                text="ғᴏᴜɴᴅᴇʀ", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="💘𝐆ɾσυ𝐏💘", url=config.SUPPORT_GROUP
+                text="ɢʀᴏᴜᴘ", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="💞𝐔ρ∂αтє𝐒💞", url=f"https://t.me/The_Incricible"
+                text="ᴜᴘᴅᴀᴛᴇs", url=config.SUPPORT_GROUP
             )
         ],
      ]
