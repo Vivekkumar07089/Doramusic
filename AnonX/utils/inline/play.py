488b753bcd602c2d4d2794fc7dbc58ca5b0bfa61
Vivekkumar07089 <155367948+Vivekkumar07089@users.noreply.github.com>
2024-01-08 14:41:44 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -211,7 +211,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f" {dur} {bar} {played} ",
                 callback_data="GetTimer",
             )
         ],
@@ -445,7 +445,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{dur} {bar} {played} ",
                 callback_data="GetTimer",
             )
         ],
